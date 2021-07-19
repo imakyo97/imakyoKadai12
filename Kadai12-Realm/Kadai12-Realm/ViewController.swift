39d905894d530dd3cb976a0c98d54e4301737e87
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-07-19 14:44:30 +0900

    Realmで課題実装

---
+++ Kadai12-Realm/Kadai12-Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai12-Realm
+//
+//  Created by 今村京平 on 2021/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
